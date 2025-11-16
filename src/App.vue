d01e510f249c979225a7884b64d3f67a396cfad9
ShmortGuac <aiman17hakimi@gmail.com>
2025-11-16 22:04:12 +0800

    Changed Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="flex flex-col justify-center items-center border-b-4 p-4 gap-4">
-    <h1 class="text-4xl font-bold">Sorting Visualiser</h1>
+    <h1 class="text-4xl font-bold">Radix Sort Visualiser</h1>
     <div class="flex flex-row gap-5 justify-center items-center">
       <button class="border-2 px-4 py-2 cursor-pointer" @click="radixSortReal(array, delay)">Radix Sort Real Time</button>
       <button class="border-2 px-4 py-2 cursor-pointer" @click="radixSortVisual(array, delay)">Radix Sort</button>
